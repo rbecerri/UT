21a11b55dd073f8d32b7bc5d68512cca3f6540b2
rbecerri <rbecerri@hotmail.com>
2018-10-13 13:57:52 -0500

    Web Scraping

---
+++ 13-Web-Scraping-and-Document-Databases/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect, jsonify
+from flask_pymongo import PyMongo
+import mission_to_mars
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+
+
+@app.route("/")
+def index():
+    marsdata = mongo.db.marsdata.find_one()
+    return render_template("index.html", marsdata=marsdata)
+
+@app.route("/scrape")
+def scraper():
+    mongo.db.marsdata.drop()
+    results = mission_to_mars.scrape()
+    mongo.db.marsdata.insert_one(results)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
